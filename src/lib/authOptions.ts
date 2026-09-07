ec6eab85e5e410fe87369fd3b176b9abd7af427d
martinz36 <martinz36@users.noreply.github.com>
2026-09-06 22:30:24 -0500

    feat: integrate Google OAuth Social Login in NextAuth and CheckoutAuthModal

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -15,14 +15,11 @@ export const authOptions: NextAuthOptions = {
     newUser: "/account/register",
   },
   providers: [
-    ...(process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET
-      ? [
-          GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-          }),
-        ]
-      : []),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      allowDangerousEmailAccountLinking: true,
+    }),
 
     CredentialsProvider({
       name: "credentials",
